c50e50d2ea0ab8ad11fde1592cc35f76727b38db
tranhd98 <62961553+tranhd98@users.noreply.github.com>
2020-09-16 14:51:42 -0700

    finished app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) =>{
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello CSC 3221 World, you are the best');
+});
+
+server.listen(port, (err) =>{
+  if (err){
+      return console.log('Something went wrong ', err);
+  }
+  console.log(`Server Listening on: ${port}`);
+});
