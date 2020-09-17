bbf411e6e910270bd60908c0385965bf6fd59d3d
tranhd98 <62961553+tranhd98@users.noreply.github.com>
2020-09-17 13:30:56 -0700

    updated app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const server = http.createServer((request, response) =>{
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/html');
-  response.sendFile(mypage.html);
+  response.sendFile('mypage.html');
 });
 
 server.listen(port, (err) =>{
